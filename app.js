8548e71320ce8b6f35c2400ccf07d123c1ff6084
saidmtanzania <saidmtanzania@gmail.com>
2022-04-19 16:04:39 +0300

    final changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser:true});
+mongoose.connect('mongodb+srv://admin-saidmtanzania:said1561@cluster0.2uzik.mongodb.net/todolistDB',{useNewUrlParser:true});
 const itemSchema ={
   name:String
 };
@@ -94,6 +94,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
